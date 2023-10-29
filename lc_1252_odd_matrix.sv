561708ac27fc9c04d0bddd8fbe9ba1aeadbf87ba
AlanChavez150 <achavez1899@gmail.com>
2023-10-29 11:57:58 -0500

    Finishes odd cell counter leetcode question

--- lc_1252_odd_matrix.sv
+++ lc_1252_odd_matrix.sv
@@ -119,36 +119,40 @@ module lc_1252_odd_matrix #(
     end
 
     // Memory space used for a matrix\
-    logic [7:0] mat_in_data;
-    logic [7:0] mat_addr;
-    logic       mat_en;
-    logic       mat_we;
+    wire  [7:0] mat_in_data_a,  mat_in_data_b;
+    logic [7:0] mat_addr_a,     mat_addr_b;
+    logic       mat_en_a,       mat_en_b;
+    logic       mat_we_a,       mat_we_b;
 
-    wire  [7:0] mat_out_data;
+    wire  [7:0] mat_out_data_a, mat_out_data_b;
 
     mem #(
         .DATA_WIDTH(8),
         .ADDR_WIDTH(8)
     ) matrix_mem (
-        .clk       ( clk          ),
-        .rst       ( rst          ),
-        .in_data_a ( mat_in_data  ),
-        .in_addr_a ( mat_addr     ),
-        .en_a      ( mat_en       ),
-        .we_a      ( mat_we       ),
-        .out_data_a( mat_out_data ),
+        .clk       ( clk            ),
+        .rst       ( rst            ),
+        .in_data_a ( mat_in_data_a  ),
+        .in_addr_a ( mat_addr_a     ),
+        .en_a      ( mat_en_a       ),
+        .we_a      ( mat_we_a       ),
+        .out_data_a( mat_out_data_a ),
         // ignore b port
-        .in_data_b ( 8'd0 ),
-        .in_addr_b ( 8'd0 ),
-        .en_b      ( 1'd0 ),
-        .we_b      ( 1'd0 ),
-        .out_data_b()
+        .in_data_b ( mat_in_data_b  ),
+        .in_addr_b ( mat_addr_b     ),
+        .en_b      ( mat_en_b       ),
+        .we_b      ( mat_we_b       ),
+        .out_data_b( mat_out_data_b )
     );
 
     // state machines for actual calculation
     // There are 2 state machines
     //  1. iterates through indicies and incremnets row and col values
     //  2. goes matrix and counts odd cells
+
+    //
+    // First state machine. increments cells as needed.
+    //
 
     enum logic [3:0]{
         INC_IDLE    = 0, // does nothing until ind_loaded signal goes high
@@ -157,81 +161,81 @@ module lc_1252_odd_matrix #(
         INC_DONE    = 3
     } inc_state;
 
-    wire [7:0] curr_index = (ind_ptr > 0) ? ind_ptr: 0;
-    wire [15:0] curr_ind  = indices[curr_index];
+    wire [ 7:0] curr_index = ind_ptr-1;
+    wire [15:0] curr_ind   = indices[curr_index];
 
-    logic [7:0] ind_cnt;
     logic [7:0] row_cnt;
     logic [7:0] col_cnt;
-    logic       read_rdy;
+    logic [7:0] state_cnt;
 
-    wire   last_row  = row_cnt == m_reg-1;
-    wire   last_col  = col_cnt == n_reg-1;
-    wire   last_cell = last_row && last_col;
-    assign mat_we = read_rdy;
+    wire   last_row      = row_cnt == m_reg-1;
+    wire   last_col      = col_cnt == n_reg-1;
+    wire   last_cell     = last_row && last_col;
+    wire   read_rdy      = state_cnt >= 2;
+    assign mat_we_a      = (inc_state != INC_DONE) ? read_rdy: 0;
+    assign mat_in_data_a = (inc_state == INC_ROW_INC || inc_state == INC_COL_INC) ?
+                         (read_rdy) ? mat_out_data_a+1: 0
+                           :0;
 
     always @(posedge clk) begin
         if(rst) begin
             inc_state <= INC_IDLE;
             row_cnt <= 0;
             col_cnt <= 0;
-            read_rdy <= 0;
-            ind_cnt <= 0;
-            mat_addr <= 0; // used in this block so its reset in this block
-            mat_in_data <= 0;
-            mat_en <= 0;
+            mat_addr_a <= 0; // used in this block so its reset in this block
+            mat_en_a <= 0;
+            state_cnt <= 0;
         end else begin
             case (inc_state)
             INC_IDLE: begin
-                if(ind_loaded) begin
-                    inc_state <= INC_ROW_INC;
-                    ind_cnt <= ind_ptr;
-                end
+                if(ind_loaded) inc_state <= INC_ROW_INC;
             end
             INC_ROW_INC: begin
-                if(curr_ind[0] == row_cnt) begin
+                if(curr_ind[7:0] == row_cnt) begin
                     // read mem, increment, and write back
                     // state machine should spend three cycles in the if statement
                     // 1. read request
                     // 2. read data, write back incremented, move on
-                    mat_addr <= addr_calc(row_cnt, col_cnt);
-                    mat_en <= 1;
-                    read_rdy <= !read_rdy;
+                    mat_addr_a <= addr_calc(row_cnt, col_cnt);
+                    mat_en_a <= 1;
                     if(read_rdy) begin
-                        mat_in_data <= mat_out_data + 1;
+                        state_cnt <= 0;
                         inc_state <= INC_COL_INC;
+                    end else begin
+                        state_cnt <= state_cnt + 1;
                     end
                 end else begin
                     // move to next state
+                    state_cnt <= 0;
                     inc_state <= INC_COL_INC;
                 end
             end
             INC_COL_INC:
-                if(curr_ind[1] == col_cnt) begin
+                if(curr_ind[15:8] == col_cnt) begin
                     // read mem, increment, and write back
                     // state machine should spend three cycles in the if statement
                     // 1. read request
                     // 2. read data, write back incremented, move on
-                    mat_addr <= addr_calc(row_cnt, col_cnt);
-                    mat_en <= 1;
-                    read_rdy <= !read_rdy;
+                    mat_addr_a <= addr_calc(row_cnt, col_cnt);
+                    mat_en_a <= 1;
                     if(read_rdy) begin
-                        mat_in_data <= mat_out_data + 1;
                         inc_col_next_state(last_row, last_col, last_cell);
+                    end else begin
+                        state_cnt <= state_cnt + 1;
                     end
                 end else begin
                     // move to next state
                     inc_col_next_state(last_row, last_col, last_cell);
                 end
             INC_DONE: begin
-
             end
             endcase
         end
     end
 
     task inc_col_next_state(input last_row, input input_col, input last_cell);
-        if(last_cell && ind_cnt == 0) begin
+        state_cnt <= 0;
+        if(last_cell && ind_ptr == 0) begin
             row_cnt <= 0;
             col_cnt <= 0;
             inc_state <= INC_DONE;
@@ -239,7 +243,7 @@ module lc_1252_odd_matrix #(
         else if (last_cell) begin
             row_cnt <= 0;
             col_cnt <= 0;
-            ind_cnt <= ind_cnt - 1;
+            ind_ptr <= ind_ptr - 1;
             inc_state <= INC_ROW_INC;
         end
         else if(last_row) begin
@@ -256,6 +260,69 @@ module lc_1252_odd_matrix #(
         // column size * row + col
        return n_reg*row + col;
     endfunction
+
+    //
+    // second state machine. Counts how many odd cells there are
+    //
+
+    // state machine that counts how many odd cells there are.
+    enum logic [3:0]{
+        ODD_IDLE = 0,
+        ODD_CNT  = 1,
+        ODD_DONE = 2
+    } odd_state;
+
+    logic [7:0] odd_cnt;
+    logic [7:0] odd_addr;
+    logic [7:0] read_delay;
+    wire        read_done;
+    wire  [7:0] max_addr;
+
+    assign mat_we_b  = 0; // read only
+    assign max_addr  = (n_reg * m_reg) - 1;
+    assign read_done = odd_addr > max_addr;
+    assign mat_en_b  = (odd_state == ODD_CNT && !read_done);
+
+    // module outputs
+    assign odd_cells = odd_cnt;
+    assign out_tvalid = odd_state == ODD_DONE;
+
+    always @(posedge clk) begin
+        if(rst) begin
+            mat_addr_b <= 0;
+            odd_state <= ODD_IDLE;
+            odd_addr <= 0;
+            odd_cnt <= 0;
+            read_delay <= 0;
+        end else begin
+            case(odd_state)
+            ODD_IDLE: begin
+                odd_state <= (inc_state == INC_DONE) ? ODD_CNT: ODD_IDLE;
+            end
+            ODD_CNT: begin
+                read_delay <= read_delay + 1;
+                if(!read_done) begin
+                    mat_addr_b <= odd_addr;
+                    odd_addr <= odd_addr + 1;
+                end
+
+                if(read_delay > 1) begin
+                    if(mat_out_data_b[0]) begin
+                        odd_cnt <= odd_cnt + 1;
+                    end
+                end
+
+                // exit case
+                if(read_done && read_delay > max_addr+1) begin
+                    odd_state <= ODD_DONE;
+                end
+            end
+            ODD_DONE: begin
+
+            end
+            endcase
+        end
+    end
 
 endmodule
 
@@ -323,10 +390,70 @@ module tb_lc_1252_odd_matrix();
 
         //$monitor("state = 0x%h row 0x%h col 0x%x ind ptr 0x%h", UUT.inc_state, UUT.row_cnt, UUT.col_cnt, UUT.ind_cnt);
         wait(UUT.inc_state == 3);
+        $display("Done Incrementing Matrix cells");
+        wait(out_tvalid);
+        $display("Done counting odd cells");
+        if(odd_cells != 6) begin
+            $error("Assertion error. %d != 6", odd_cells);
+            $fatal(1);
+        end
+        $display("%d odd cells", odd_cells);
         mem_dump(0);
-        //repeat (100) @(posedge clk);
+
+        // reset and insert other test case
+        rst = 1;
+        clk = 0;
+        m = 0;
+        n = 0;
+        ind_tdata = 0;
+        ind_tvalid = 0;
+        ind_tlast = 0;
+        repeat(5) @(posedge clk);
+        rst = 0;
+        wait(ind_tready);
+        $display("Sending in data. 2x2 [[1,1],[0,0]]");
+        m = 2;
+        n = 2;
+        send_coor(1, 1, 0);
+        send_coor(0, 0, 1);
+        ind_tvalid = 0;
+        repeat(1) @(posedge clk);
+        if(!UUT.ind_loaded || UUT.m_reg != m || UUT.n_reg != n) begin
+            $error("Assertion error");
+            $fatal(1);
+        end
+        print_ind(0);
+
+        //$monitor("state = 0x%h row 0x%h col 0x%x ind ptr 0x%h", UUT.inc_state, UUT.row_cnt, UUT.col_cnt, UUT.ind_cnt);
+        wait(UUT.inc_state == 3);
+        $display("Done Incrementing Matrix cells");
+        wait(out_tvalid);
+        $display("Done counting odd cells");
+        if(odd_cells != 0) begin
+            $error("Assertion error. %d != 0", odd_cells);
+            $fatal(1);
+        end
+        $display("%d odd cells", odd_cells);
+        mem_dump(0);
+
         $finish;
     end
+
+    // watch mem writes
+    //always @(posedge clk) begin
+    //    if(UUT.matrix_mem.we_a && UUT.matrix_mem.en_a) begin
+    //        $display("Read 0x%h from 0x%h", UUT.matrix_mem.out_data_a, UUT.matrix_mem.in_addr_a);
+    //        $display("Writing 0x%h to 0x%h", UUT.matrix_mem.in_data_a, UUT.matrix_mem.in_addr_a);
+    //        if(UUT.inc_state == UUT.INC_ROW_INC) begin
+    //            $display("ROW write");
+    //        end else if(UUT.inc_state == UUT.INC_COL_INC) begin
+    //            $display("COL write");
+    //        end
+    //        $display("Curr index 0x%h ind ptr 0x%h", UUT.curr_index, UUT.ind_ptr);
+    //        $display("curr ind (0x%h, 0x%h). cnts (0x%h, 0x%h)", UUT.curr_ind[7:0], UUT.curr_ind[15:8], UUT.row_cnt, UUT.col_cnt);
+    //        $display("");
+    //    end
+    //end
 
     // TASKS
     task print_ind(input dummy);
